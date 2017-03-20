500ef535c9bddc49bcc18a2f01c0ac7d4bb6a44f
Paulius Imbrasas <cremboc@gmail.com>
2017-03-20 12:33:23 +0000

    Improve code structure of the enable annotation

--- macros/src/main/scala/vista/enable.scala
+++ macros/src/main/scala/vista/enable.scala
@@ -1,14 +1,13 @@
 package vista
 
-import scala.annotation.StaticAnnotation
-import scala.meta.Term.Block
-import scala.meta._
 import vista.helpers.OpHelpers._
 import vista.modifiers._
 import vista.operations._
-import vista.operations.parsers.OpOverload
 
+import scala.annotation.StaticAnnotation
 import scala.collection.immutable.Seq
+import scala.meta.Term.Block
+import scala.meta._
 
 class enable extends StaticAnnotation {
 
@@ -24,8 +23,11 @@ class enable extends StaticAnnotation {
           case t: Defn.Trait => db.addClass(t)
         }
 
-        defn.collect(semDbBuilder)
+        defn.traverse(semDbBuilder)
 
+        // all modifiers that can expand a [A <: Tree] into a Term.Block
+        // merge them into a single partial function so we can apply them at
+        // the "same time"
         val modifiers = Seq(
           ForbidModifiers.defnValModifier,
           UnionModifiers.defnValModifier,
@@ -33,40 +35,42 @@ class enable extends StaticAnnotation {
           ProductModifiers.defnValModifier
         ).reduce(_ orElse _)
 
+        // if non of the modifiers match, we simply convert
+        // a single statement into a term.block so we can use
+        // flatMap later on
+        val default: PartialFunction[Tree, Term.Block] = {
+          case o: Stat => Term.Block(Seq(o))
+        }
+
+        // the final function that is used to expand ops
+        // if none of the modifiers match, we used the default.
+        // after the modifiers are applied, we add the newly generated
+        // classes into the semdb so we can operate on them as well
+        val function = modifiers orElse default andThen { c => c.traverse(semDbBuilder); c }
+
         val Block(nstats) = Block(stats)
           .transform { // first convert all classes into traits
             case classdefn: Defn.Class => Tratify(classdefn)
           }
           .transform {
+            // since we converted classes into traits we need to make sure they are instantiable
             case v: Defn.Val => v.rhs match {
-                case rhs: Term.New =>
-                  val nCtors = rhs.templ.parents :+ Ctor.Name("vistas.AnyV")
-                  v.copy(rhs = rhs.copy(rhs.templ.copy(parents = nCtors)))
-                case _ => v
-              }
+              case rhs: Term.New =>
+                val nCtors = rhs.templ.parents :+ Ctor.Name("vistas.AnyV")
+                v.copy(rhs = rhs.copy(rhs.templ.copy(parents = nCtors)))
+              case _ => v
+            }
 
+            // if a term block as an op, we will expand it
             case b: Term.Block if hasOp(b) =>
-              println(db.classes.map(_.name))
-
-
-              val default: PartialFunction[Tree, Term.Block] = {
-                case o: Stat => Term.Block(Seq(o))
-              }
-              val function = modifiers orElse default andThen { c =>
-                c.collect(semDbBuilder)
-                c
-              }
               val modified = b.stats.collect(function).flatMap(_.stats)
-
-              println(modified)
-
               Term.Block(modified)
           }
 
         val ntemplate = obj.templ.copy(stats = Option(nstats))
         obj.copy(templ = ntemplate)
       case _ =>
-        abort("Only allowed on objects for now.")
+        abort("Only objects are supported")
     }
   }
 }
