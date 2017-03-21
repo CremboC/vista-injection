637c2342a594ad8627e59e89254f4a83ba2029b5
Paulius Imbrasas <cremboc@gmail.com>
2017-03-21 08:31:03 +0000

    Potentially provide alternative syntax for operators

--- core/src/main/scala/vistas/Vista.scala
+++ core/src/main/scala/vistas/Vista.scala
@@ -3,13 +3,21 @@ package vistas
 trait AnyV
 sealed trait Vista extends AnyV
 
-trait Union extends Vista
+trait Union        extends Vista
 trait Intersection extends Vista
-trait Difference extends Vista
-trait Product extends Vista
+trait Difference   extends Vista
+trait Product      extends Vista
 
 object Vista {
-  //  union
+
+  implicit class ops[A](self: A) {
+    def ∪[B](other: B) = throw new RuntimeException("Should be compiled out")
+    def ∩[B](right: B) = throw new RuntimeException("Should be compiled out")
+    def ∖(arg: {})    = throw new RuntimeException("Should be compiled out")
+    def ⨯[B](right: B) = throw new RuntimeException("Should be compiled out")
+  }
+
+  // union
   def ∪[A, B](left: A, right: B): Any = throw new RuntimeException("Should be compiled out")
 
   // intersection
